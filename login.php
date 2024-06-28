e3e1dab6f4894e897be6052cefcfdc016b78c1ef
VitoMarino <marino.vito2001@gmail.com>
2024-06-28 17:02:09 +0200

    commit

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 
 if(isset($_GET["userName"]) && isset($_GET["password"])) {
     $_SESSION["userName"] = $_GET["userName"];
-    $_SESSION["password"] = $_GET["password"] ?>
+    $_SESSION["password"] = $_GET["password"]; ?>
         
         <h1>
             Benvenuto <?php echo $_SESSION["userName"]; ?>
